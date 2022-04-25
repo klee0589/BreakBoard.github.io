d72de053ffbe94716047f2885bb703ed973f0223
Daniel <daniel@arthur.ai>
2022-04-25 00:07:36 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -5,28 +5,13 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 function App() {
-  const sports = [
-    { name: "F1", value: "f1" },
-    { name: "NFL", value: "nfl" },
-    { name: "SOCCER", value: "soc" },
-  ];
-
   return (
     <div className="App">
       <Navbar variant="dark" expand="lg" className={"navbar"}>
         <Container>
           <Navbar.Brand href="#home">Breaker Board 5000</Navbar.Brand>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown" onSelect={() => console.log("CHANGE SPORT")}>
-            {sports.map((sport) => (
-              <NavDropdown.Item key={sport.value}>
-                {sport.name}
-              </NavDropdown.Item>
-            ))}
-            {/* <NavDropdown.Item onSelect={() => console.log('CHANGE SPORT')}>F1</NavDropdown.Item> */}
-          </NavDropdown>
         </Container>
       </Navbar>
       <Container>
--- src/features/teamBoard/TeamBoard.js
+++ src/features/teamBoard/TeamBoard.js
@@ -5,10 +5,11 @@ import styles from "./TeamBoard.module.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import { f1_teams } from "./data";
+import { f1_teams, nfl_teams } from "./data";
 import Dice from "react-dice-roll";
 
 export function TeamBoard() {
@@ -17,11 +18,20 @@ export function TeamBoard() {
 
   const [count, setCount] = useState(0);
 
+  // const [activeSports, setSport] = useState('f1');
+
+  // const sports = [
+  //   { name: "F1", value: "f1", isActive: true },
+  //   { name: "NFL", value: "nfl", isActive: false },
+  //   { name: "SOCCER", value: "soc", isActive: false },
+  // ];
+
   return (
     <Container>
       <Row>
         <Col xs={3}>
           <ListGroup variant="flush">
+            
             {f1_teams.map((team) => (
               <ListGroup.Item key={team.id}>
                 <div className={styles.teamContainer}>
@@ -43,11 +53,11 @@ export function TeamBoard() {
                       <Form.Control
                         type="text"
                         placeholder={team.owner ? team.owner : index}
-                        onChange={(e) =>
-                          dispatch(
-                            addOwner({ id: team.id, owner: e.target.value })
-                          )
-                        }
+                        // onChange={(e) =>
+                        //   dispatch(
+                        //     addOwner({ id: team.id, owner: e.target.value })
+                        //   )
+                        // }
                       />
                     </Form.Group>
                   </Form>
@@ -56,18 +66,32 @@ export function TeamBoard() {
             ))}
           </ListGroup>
         </Col>
-        <Col xs={2}>
-          <Button
-            size="lg"
-            onClick={() => {
-              dispatch(shuffle());
-              setCount(count + 1);
-            }}
-          >
-            {count > 0 ? `Randomized ${count}X` : "Randomize"}
-          </Button>
-        </Col>
         <Col xs={4}>
+          <ButtonGroup>
+            <Button
+              variant="outline-success"
+              size="md"
+              onClick={() => {
+                dispatch(shuffle());
+                setCount(count + 1);
+              }}
+            >
+              {count > 0 ? `Randomized ${count}X` : "Randomize"}
+            </Button>
+            {/* {sports.map((sport) => (
+              <Button
+                key={sport.value}
+                variant="outline-primary"
+                active={sport.isActive}
+                value={sport.value}
+                onClick={(e) => setSport(e.target.value)}
+              >
+                {sport.name}
+              </Button>
+            ))} */}
+          </ButtonGroup>
+        </Col>
+        <Col xs={2}>
           <Row>
             <Col>
               <Dice
